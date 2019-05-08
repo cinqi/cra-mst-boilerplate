195cc6625477e496f9fca21bd360e923db505ec9
Tuomas Rissanen <tuomas.rissanen@gmail.com>
2019-05-08 12:38:18 +0300

    Added placeholders to manually edit stores.js

--- src/stores.js
+++ src/stores.js
@@ -1,12 +1,20 @@
-// Do not edit this line
+/*
+This file is updated automatically when model is generated.
+Edit only the parts that are marked editable.
+ */
 import globalViewStore from './models/View/GlobalViewStore';
-// Can edit below this line
 
-const stores = {
-  // Do not edit this part!
+// ---------------------------------
+// If you want to manually import something, add imports between ---- lines
+
+// ---------------------------------
+
+// Do not edit this part!
+const scriptImportedStores = {
   globalViewStore,
-  // Do not edit this part!
-  // Can edit below this
 };
 
+// If you want to manually add store, add it here
+const manuallyAddedStores = {};
+const stores = { ...scriptImportedStores, ...manuallyAddedStores };
 export default stores;
